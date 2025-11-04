7f0dac0e1d01a124a50d4737e35dfc6df117772c
Hafizhputra27 <hafizh.putra@widyatama.ac.id>
2025-11-04 10:58:20 +0700

    Menambahkan file js dan update html & css

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Menampilkan alert saat tombol diklik
+function showMessage() {
+  alert("Halo! Ini adalah interaksi dari JavaScript.");
+}
+
+// Mengubah isi teks paragraf
+function changeText() {
+  document.getElementById("info").innerText = "Teks ini telah diubah oleh JavaScript!";
+}
